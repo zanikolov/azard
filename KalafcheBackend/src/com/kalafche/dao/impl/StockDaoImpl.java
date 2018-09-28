657a9d678d52c4104c4578cd438bc8f8d6665ba9
Zahari.Nikolov <zahari.nikolov@ezbob.com>
2018-09-28 14:41:05 +0300

    Changes in InStock view - fix.

--- KalafcheBackend/src/com/kalafche/dao/impl/StockDaoImpl.java
+++ KalafcheBackend/src/com/kalafche/dao/impl/StockDaoImpl.java
@@ -100,14 +100,14 @@ public class StockDaoImpl extends JdbcDaoSupport implements StockDao {
 			"s.approved, " +
 			"s.approver, " +
 			"ws.quantity as extraQuantity, " +
-			"sr2.quantity as orderedQuantity " +
+			"sum(sr2.quantity) as orderedQuantity " +
 			"from stock s " +
 			"join device_model dm on s.DEVICE_MODEL_ID=dm.ID " +
 			"join device_brand db on dm.DEVICE_BRAND_ID=db.ID " +
 			"join kalafche_store ks on s.KALAFCHE_STORE_ID=ks.ID " +
 			"join item i on s.ITEM_ID=i.ID " +
 			"left join stock ws on ws.device_model_id=dm.ID and ws.item_id=i.ID and ws.kalafche_store_id=4 and ws.approved=true " +
-			"left join (select sr1.quantity, sr1.to_kalafche_store_id, st3.item_id, st3.device_model_id from stock_relocation sr1 join stock st3 on sr1.stock_id=st3.id where sr1.from_kalafche_store_id=4 and sr1.arrived=false and sr1.archived=false and sr1.rejected=false) sr2 on sr2.item_id=i.id and sr2.device_model_id=dm.id and ks.id=sr2.to_kalafche_store_id " +
+			"left join (select sr1.quantity, sr1.to_kalafche_store_id, st3.item_id, st3.device_model_id from stock_relocation sr1 join stock st3 on sr1.stock_id=st3.id where sr1.from_kalafche_store_id=4 and sr1.arrived=false and sr1.archived=false and (sr1.rejected=false or sr1.rejected is null)) sr2 on sr2.item_id=i.id and sr2.device_model_id=dm.id and ks.id=sr2.to_kalafche_store_id " +
 			"where s.approved is true " +
 			"and ks.CODE <> 'RU_WH' ";
 	
@@ -116,6 +116,9 @@ public class StockDaoImpl extends JdbcDaoSupport implements StockDao {
 	
 	private static final String BY_STORE_CLAUSE = 		
 			"and ks.ID = ? ";
+	
+	private static final String GROUP_BY_CLAUSE = 	
+	"group by s.id ";
 			
 //			"union all " +
 	
@@ -135,16 +138,18 @@ public class StockDaoImpl extends JdbcDaoSupport implements StockDao {
 			"s.approved, " +
 			"s.approver, " +
 			"es.quantity as extraQuantity, " +
-			"sr2.quantity as orderedQuantity " +
+			"sum(sr2.quantity) as orderedQuantity " +
 			"from stock s " +
 			"join device_model dm on s.DEVICE_MODEL_ID=dm.ID " +
 			"join device_brand db on dm.DEVICE_BRAND_ID=db.ID " +
 			"join kalafche_store ks on s.KALAFCHE_STORE_ID=ks.ID " +
 			"join item i on s.ITEM_ID=i.ID " +
 			"left join stock es on es.device_model_id=dm.ID and es.item_id=i.ID and es.kalafche_store_id=? and es.approved=true " +
-			"left join (select sr1.quantity, sr1.to_kalafche_store_id, st3.item_id, st3.device_model_id from stock_relocation sr1 join stock st3 on sr1.stock_id=st3.id where sr1.from_kalafche_store_id=4 and sr1.arrived=false and sr1.archived=false and sr1.rejected=false) sr2 on sr2.item_id=i.id and sr2.device_model_id=dm.id and sr2.to_kalafche_store_id=? " +
+			"left join (select sr1.quantity, sr1.to_kalafche_store_id, st3.item_id, st3.device_model_id from stock_relocation sr1 join stock st3 on sr1.stock_id=st3.id where sr1.from_kalafche_store_id=4 and sr1.arrived=false and sr1.archived=false and (sr1.rejected=false or sr1.rejected is null)) sr2 on sr2.item_id=i.id and sr2.device_model_id=dm.id and sr2.to_kalafche_store_id=? " +
 			"where s.approved is true " +
-			"and ks.CODE = 'RU_WH' " +
+			"and ks.CODE = 'RU_WH' ";
+	
+	private static final String ORDER_BY_CLAUSE =
 			"order by device_brand_name, device_model_name, item_id, kalafche_store_id ";
 	
 	private static final String UPDATE_QUANTITY_OF_SOLD_STOCK = "update stock set quantity = quantity - 1 where id = ?";
@@ -269,18 +274,18 @@ public class StockDaoImpl extends JdbcDaoSupport implements StockDao {
 
 	public List<Stock> getAllApprovedStocks(int userKalafcheStoreId, int selectedKalafcheStoreId) {
 		if (selectedKalafcheStoreId == 0) {
-			List<Stock> stocks = getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FOR_STORES,
+			List<Stock> stocks = getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FOR_STORES + GROUP_BY_CLAUSE + ORDER_BY_CLAUSE,
 					getRowMapper());
-			stocks.addAll(getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FROM_WH, 
+			stocks.addAll(getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FROM_WH + GROUP_BY_CLAUSE + ORDER_BY_CLAUSE, 
 					new Object[]{userKalafcheStoreId, userKalafcheStoreId}, getRowMapper()));
 
 			return stocks;
 			
 		} else if (selectedKalafcheStoreId == 4) {
-			return getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FROM_WH, 
+			return getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FROM_WH + GROUP_BY_CLAUSE + ORDER_BY_CLAUSE, 
 					new Object[]{userKalafcheStoreId, userKalafcheStoreId}, getRowMapper());
 		} else {
-			return getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FOR_STORES + BY_STORE_CLAUSE, new Object[]{selectedKalafcheStoreId},
+			return getJdbcTemplate().query(GET_ALL_APPROVED_STOCKS_FOR_STORES + BY_STORE_CLAUSE + GROUP_BY_CLAUSE + ORDER_BY_CLAUSE, new Object[]{selectedKalafcheStoreId},
 					getRowMapper());
 		}
 	}
